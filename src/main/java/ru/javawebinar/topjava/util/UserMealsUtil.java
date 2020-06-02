739e1a1e3d3ab03d44ff0b6428a0fbe16050ca9c
ViktorKorepanov <greeenray@gmail.com>
2020-06-02 16:32:23 +0300

    Homework0 whits Stream API

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -10,6 +10,7 @@ import java.time.Month;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class UserMealsUtil {
     public static void main(String[] args) {
@@ -23,10 +24,10 @@ public class UserMealsUtil {
                 new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 31, 20, 0), "Ужин", 410)
         );
 
-        List<UserMealWithExcess> mealsTo = filteredByCycles(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
-            mealsTo.forEach(System.out::println);
+//        List<UserMealWithExcess> mealsTo = filteredByCycles(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
+//            mealsTo.forEach(System.out::println);
 
-//        System.out.println(filteredByStreams(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000));
+        System.out.println(filteredByStreams(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000));
     }
 
     public static List<UserMealWithExcess> filteredByCycles(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int maxCaloriesPerDay) {
@@ -79,7 +80,46 @@ public class UserMealsUtil {
     }
 
     public static List<UserMealWithExcess> filteredByStreams(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int maxCaloriesPerDay) {
-        // Implement by streams
-        return null;
+        List<UserMealWithExcess> resultNoFilter = new ArrayList<>();
+        List<UserMealWithExcess> resultFilter = new ArrayList<>();
+        List<UserMeal> oneDayMeals = new ArrayList<>();
+        List<Integer> caloriesPerDay = new ArrayList<>();
+        long i = 0;
+
+        do {
+            LocalDate date = meals.get((int) i).getDate();
+            meals.stream()
+                    .skip(i)
+                    .filter(m -> {
+                        if (m.getDate().equals(date)) {
+                            caloriesPerDay.add(m.getCalories());
+                            return true;
+                        } else
+                            return false;
+                    })
+                    .forEach(oneDayMeals::add);
+
+            if (caloriesPerDay.stream().reduce((acc, c) -> acc + c).get() > maxCaloriesPerDay) {
+                oneDayMeals.stream()
+                        .forEach(m -> {
+                            resultNoFilter.add(new UserMealWithExcess(m.getDateTime(), m.getDescription(), m.getCalories(), false));
+                        });
+            } else {
+                oneDayMeals.stream()
+                        .forEach(m -> {
+                            resultNoFilter.add(new UserMealWithExcess(m.getDateTime(), m.getDescription(), m.getCalories(), true));
+                        });
+            }
+
+            i += oneDayMeals.size();
+            caloriesPerDay.clear();
+            oneDayMeals.clear();
+        } while (i < meals.size());
+
+        resultNoFilter.stream()
+                .filter(m -> (TimeUtil.isBetweenHalfOpen(m.getTime(), startTime, endTime)))
+                .forEach(resultFilter::add);
+
+        return resultFilter;
     }
 }
